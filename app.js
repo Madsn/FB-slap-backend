7d4700df8f581f5c2d18c0587708eabda6ca5b70
Mikkel Madsen <m@madsn.net>
2014-11-23 21:38:46 +0100

    work in progress

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-var restify = require('restify');
+var restify = require('restify'),
+    mongoose = require('mongoose');
+
+mongoose.connect('mongodb://x:y@z');
 
 var server = restify.createServer();
 
-function sendV1(req, res, next) {
-  res.send('hello: ' + req.params.name);
-  return next();
+var SlapSchema = new mongoose.Schema({
+  id: {type: ObjectId}
+  fbid: { type: String, trim: true, required: true }
+  , count: { type: Number, required: true, default: 0 }
+});
+
+var Slap = mongoose.model('Slap', SlapSchema);
+
+function add(req, res, next) {
+  Slap.findOne({fbid: '1'}, function(err, item){
+    console.log(err, item);
+    item.count = item.count++;
+    item.update(function(err, savedItem){
+      console.log('saved:', err, savedItem);
+    });
+    console.log('post-save');
+    res.send(JSON.stringify(item.count));
+    return next();
+  });
 }
 
-function sendV2(req, res, next) {
-  res.send({hello: req.params.name});
-  return next();
+function send(req, res, next) {
+  console.log(req.params);
+  Slap.findOne({fbid: '1'}, function(err, item){
+    console.log(item);
+    console.log(item.count);
+    res.send(JSON.stringify(item.count));
+    return next();
+  });
 }
 
-var PATH = '/hello/:name';
-server.get({path: PATH, version: '2.0.0'}, sendV2);
-server.get({path: PATH, version: '1.1.3'}, sendV1);
+server.get({path: '/get/:id'}, send);
+server.get({path: '/add/:id'}, add);
 
 server.listen(8080);
