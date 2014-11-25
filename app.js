372db1c84aa7afedb9429869ea5407abbfffb3ff
Mikkel Madsen <m@madsn.net>
2014-11-25 08:40:29 +0100

    Express (yeoman generator)

--- app.js
+++ app.js
@@ -1,42 +1,63 @@
-var restify = require('restify'),
-    mongoose = require('mongoose');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://x:y@z');
+var routes = require('./routes/index');
+var users = require('./routes/user');
 
-var server = restify.createServer();
+var app = express();
 
-var SlapSchema = new mongoose.Schema({
-  id: {type: ObjectId}
-  fbid: { type: String, trim: true, required: true }
-  , count: { type: Number, required: true, default: 0 }
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-var Slap = mongoose.model('Slap', SlapSchema);
+/// error handlers
 
-function add(req, res, next) {
-  Slap.findOne({fbid: '1'}, function(err, item){
-    console.log(err, item);
-    item.count = item.count++;
-    item.update(function(err, savedItem){
-      console.log('saved:', err, savedItem);
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            title: 'error'
+        });
     });
-    console.log('post-save');
-    res.send(JSON.stringify(item.count));
-    return next();
-  });
 }
 
-function send(req, res, next) {
-  console.log(req.params);
-  Slap.findOne({fbid: '1'}, function(err, item){
-    console.log(item);
-    console.log(item.count);
-    res.send(JSON.stringify(item.count));
-    return next();
-  });
-}
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
+    });
+});
 
-server.get({path: '/get/:id'}, send);
-server.get({path: '/add/:id'}, add);
 
-server.listen(8080);
+module.exports = app;
